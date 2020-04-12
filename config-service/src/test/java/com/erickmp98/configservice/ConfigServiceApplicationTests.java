9e20f94b3a946d7dbf694e6a366a3a9df8f619d9
Erick Molleapaza <molleapaza98@gmail.com>
2020-04-12 10:36:15 -0500

    adding eureka server and config server

---
+++ config-service/src/test/java/com/erickmp98/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickmp98.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ discovery-service/src/test/java/com/erickmp98/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erickmp98.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
