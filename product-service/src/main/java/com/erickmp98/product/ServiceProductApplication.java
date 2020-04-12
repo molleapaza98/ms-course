373131d813bef5134b3ce45a43ed93437569175c
Erick Molleapaza <molleapaza98@gmail.com>
2020-04-12 15:56:09 -0500

    add new field

--- product-service/src/main/java/com/erickmp98/product/ServiceProductApplication.java
+++ product-service/src/main/java/com/erickmp98/product/ServiceProductApplication.java
@@ -2,7 +2,9 @@ package com.erickmp98.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceProductApplication {
 
